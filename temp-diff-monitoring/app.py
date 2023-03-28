eb447daf5f00f6abc732fbc48a77a5a5f128a6f4
kyleluoma <kyle@jp8.dev>
2023-03-27 19:57:10 -0700

    changed to only update weather once per 5 minutes

--- temp-diff-monitoring/app.py
+++ temp-diff-monitoring/app.py
@@ -47,12 +47,20 @@ except mariadb.Error as e:
 update_temp_db_cur = update_temp_db_conn.cursor()
 @app.route("/updatetemp/", methods = ["GET", "POST"])
 def update_temp():
+    latest_observation_sql = """SELECT timestamp FROM observations ORDER BY timestamp DESC LIMIT 1"""
+    update_temp_db_cur.execute(latest_observation_sql)
+    latest_observation = update_temp_db_cur.fetchone()
+    latest_hour = latest_observation[0][3]
+    latest_minute = latest_observation[0][4]
+    current_hour = time.localtime().tm_hour
+    current_minute = time.localtime().tm_min
     if request.method == "POST":
         print("From client", request.remote_addr)
         print(request.json['temperature'], ", ", request.json['humidity'])
         date_time = time.strftime('%Y-%m-%d %H:%M:%S')
-        update_temp_db_cur.execute(add_observation_sql, (date_time, str(request.remote_addr), request.json['temperature'], request.json['humidity']))
-        update_temp_db_conn.commit()
+        if current_minute - latest_minute > 5 or current_hour > latest_hour:
+            update_temp_db_cur.execute(add_observation_sql, (date_time, str(request.remote_addr), request.json['temperature'], request.json['humidity']))
+            update_temp_db_conn.commit()
     return "received"
 
 
